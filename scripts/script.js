15386b75c4f520e71ad6604f4fdcda854bea8079
Gagandeep9354 <sgagan9354@gmail.com>
2024-10-31 19:41:12 -0600

    Full functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 const addButton = document.getElementsByClassName("add-button")[0];
+const inputForm = document.getElementsByClassName("form")[0];
+const submitButton = document.getElementsByClassName("submit-button")[0];
 const displayList = document.getElementsByClassName("display")[0];
+const overlayDiv = document.getElementsByClassName("overlay")[0];
 const book1 = new Book("The Midnight Library", "Matt Haig", 304, false);
 const book2 = new Book("Educated: A Memoir", "Tara Westover", 352, true);
 
@@ -12,68 +15,94 @@ function Book(title, author, numPages, isRead) {
 }
 
 function addBookToLibrary (title, author, numPages, isRead) {
-    const newBook = newBook(title, author, numPages, isRead);
+    const newBook = new Book(title, author, numPages, isRead);
     myLibrary.push(newBook);
 }
-myLibrary.push(book1);
-myLibrary.push(book2);
-for (let i = 0; i < myLibrary.length; i++) {
-    const listItem = document.createElement("div");
-    listItem.classList.add("card-component");
+displayBooks(myLibrary);
 
-    const title = document.createElement("p");
-    title.textContent = `Title: ${myLibrary[i].title}`;
+function displayBooks(myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const listItem = document.createElement("div");
+        listItem.classList.add("card-component");
 
-    const author = document.createElement("p");
-    author.textContent = `Author: ${myLibrary[i].author}`;
+        const title = document.createElement("p");
+        title.textContent = `Title: ${myLibrary[i].title}`;
 
-    const numPages = document.createElement("p");
-    numPages.textContent = `Number of Pages: ${myLibrary[i].numPages}`;
+        const author = document.createElement("p");
+        author.textContent = `Author: ${myLibrary[i].author}`;
 
-    const isRead = document.createElement("p");
-    isRead.textContent = `Is Read: ${myLibrary[i].isRead ? 'Read' : 'Not read yet'}`;
-    isRead.style.color = myLibrary[i].isRead ? 'green' : 'red';
+        const numPages = document.createElement("p");
+        numPages.textContent = `Number of Pages: ${myLibrary[i].numPages}`;
 
-    const buttonsDiv = document.createElement("div");
-    buttonsDiv.classList.add("buttons");
+        const isRead = document.createElement("p");
+        isRead.textContent = `Is Read: ${myLibrary[i].isRead ? 'Read' : 'Not read yet'}`;
+        isRead.style.color = myLibrary[i].isRead ? 'green' : 'red';
 
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("button");
-    removeButton.style.backgroundColor = 'red';
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-        myLibrary.splice(i, 1);
-        displayList.removeChild(listItem);
-    });
+        const buttonsDiv = document.createElement("div");
+        buttonsDiv.classList.add("buttons");
 
-    const toggleReadButton = document.createElement("button");
-    toggleReadButton.classList.add("button");
-    toggleReadButton.style.backgroundColor = myLibrary[i].isRead ? 'red' : 'green';
-    toggleReadButton.textContent = myLibrary[i].isRead ? 'Not Read' : 'Read';
-    toggleReadButton.addEventListener('click', () => {
-        myLibrary[i].isRead = !myLibrary[i].isRead;
-        if (myLibrary[i].isRead == true) {
-            toggleReadButton.style.backgroundColor = "red";
-            toggleReadButton.textContent = "Not Read";
-            isRead.textContent = "Is Read: Read";
-            isRead.style.color = "green";
-        } else {
-            toggleReadButton.style.backgroundColor = "green";
-            toggleReadButton.textContent = "Read";
-            isRead.textContent = "Is Read: Not read yet";
-            isRead.style.color = "red";
-        }
-    })
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("button");
+        removeButton.style.backgroundColor = 'red';
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', () => {
+            myLibrary.splice(i, 1);
+            displayList.removeChild(listItem);
+        });
 
-    //appending child elements to their parent component
-    listItem.appendChild(title);
-    listItem.appendChild(author);
-    listItem.appendChild(numPages);
-    listItem.appendChild(isRead);
-    buttonsDiv.appendChild(removeButton);
-    buttonsDiv.appendChild(toggleReadButton);
-    listItem.appendChild(buttonsDiv);
+        const toggleReadButton = document.createElement("button");
+        toggleReadButton.classList.add("button");
+        toggleReadButton.style.backgroundColor = myLibrary[i].isRead ? 'red' : 'green';
+        toggleReadButton.textContent = myLibrary[i].isRead ? 'Not Read' : 'Read';
+        toggleReadButton.addEventListener('click', () => {
+            myLibrary[i].isRead = !myLibrary[i].isRead;
+            if (myLibrary[i].isRead == true) {
+                toggleReadButton.style.backgroundColor = "red";
+                toggleReadButton.textContent = "Not Read";
+                isRead.textContent = "Is Read: Read";
+                isRead.style.color = "green";
+            } else {
+                toggleReadButton.style.backgroundColor = "green";
+                toggleReadButton.textContent = "Read";
+                isRead.textContent = "Is Read: Not read yet";
+                isRead.style.color = "red";
+            }
+        })
 
-    displayList.appendChild(listItem);
-    
+        //appending child elements to their parent component
+        listItem.appendChild(title);
+        listItem.appendChild(author);
+        listItem.appendChild(numPages);
+        listItem.appendChild(isRead);
+        buttonsDiv.appendChild(removeButton);
+        buttonsDiv.appendChild(toggleReadButton);
+        listItem.appendChild(buttonsDiv);
+
+        displayList.appendChild(listItem);    
+    }
 }
+
+addButton.addEventListener('click', () => {
+    inputForm.style.display = "flex";
+    overlayDiv.style.display = "block";
+})
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const formData = new FormData(inputForm);
+    const data = {};
+    let isReadBool = false;
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    if (data.isRead == "on") {
+        isReadBool = true;
+    } else {
+        isReadBool = false;
+    }
+    addBookToLibrary(data.title, data.author, data.numPages, isReadBool);
+    displayBooks(myLibrary);
+    inputForm.style.display = "none";
+    overlayDiv.style.display = "none";
+    inputForm.reset();
+})
